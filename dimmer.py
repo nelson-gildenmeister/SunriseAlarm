100078b55934f4b4558a820dd8cdf87a53a99b39
nelson-gildenmeister <nelson.gildenmeister@gmail.com>
2025-06-26 09:56:15 -0600

    fix increment_level()

--- dimmer.py
+++ dimmer.py
@@ -5,14 +5,14 @@ import pigpio
 
 class Dimmer:
     __frequency__: int = 1000
-    __max_dutycycle__: int = 255
-    __min_dutycycle__: int = 0
-    __num_steps__: int = __max_dutycycle__ - __min_dutycycle__
+    __max_duty_cycle__: int = 255
+    __min_duty_cycle__: int = 0
+    __num_steps__: int = __max_duty_cycle__ - __min_duty_cycle__
 
     def __init__(self):
         self.is_enabled: bool = False
         self.pwm_gpio = 13
-        self.dutycycle: int = 0
+        self.duty_cycle: int = 0
         self.pi = pigpio.pi()
         self.pi.set_PWM_frequency(self.pwm_gpio, self.__frequency__)
         self.pi.set_PWM_dutycycle(self.pwm_gpio, 0)
@@ -26,19 +26,19 @@ class Dimmer:
         :return: None
         """
         if self.is_enabled:
-            if 0 <= level <= self.__max_dutycycle__:
-                self.dutycycle = level
+            if 0 <= level <= self.__max_duty_cycle__:
+                self.duty_cycle = level
                 self.pi.set_PWM_dutycycle(self.pwm_gpio, level)
             else:
                 raise ValueError
         else:
             # Not enabled, set to off
-            if self.dutycycle > 0:
-                self.dutycycle = 0
-                self.pi.set_PWM_dutycycle(self.pwm_gpio, self.dutycycle)
+            if self.duty_cycle > 0:
+                self.duty_cycle = 0
+                self.pi.set_PWM_dutycycle(self.pwm_gpio, self.duty_cycle)
 
     def get_level(self) -> int:
-        return self.dutycycle
+        return self.duty_cycle
 
     def enable(self):
         self.is_enabled = True
@@ -50,36 +50,33 @@ class Dimmer:
         return self.is_enabled
 
     def shutdown(self):
-        self.pi.set_PWM_dutycycle(self.pwm_gpio, self.dutycycle)
+        self.pi.set_PWM_dutycycle(self.pwm_gpio, self.duty_cycle)
         self.pi.set_PWM_frequency(self.pwm_gpio, 0)
         self.pi.stop()
 
     def get_max_level(self) -> int:
-        return self.__max_dutycycle__
+        return self.__max_duty_cycle__
 
     def get_min_level(self) -> int:
-        return self.__min_dutycycle__
+        return self.__min_duty_cycle__
 
     def turn_off(self):
         self.pi.set_PWM_dutycycle(self.pwm_gpio, 0)
 
     def turn_on(self):
-        self.pi.set_PWM_dutycycle(self.pwm_gpio, self.__max_dutycycle__)
+        self.pi.set_PWM_dutycycle(self.pwm_gpio, self.__max_duty_cycle__)
 
     def get_num_steps(self) -> int:
         return self.__num_steps__
 
+    # Positive or negative change in brightness level.  Returns False if unable to change the
+    # brightness level due to already being at maximum or minimum.
     def increment_level(self, steps: int = 1) -> bool:
-        if self.dutycycle < self.__max_dutycycle__:
-            self.dutycycle = self.dutycycle + steps
-            if self.dutycycle > self.__max_dutycycle__:
-                self.dutycycle = self.__max_dutycycle__
+        new_duty_cycle = self.duty_cycle + steps
+        if new_duty_cycle > self.__max_duty_cycle__ or new_duty_cycle < self.__min_duty_cycle__:
+            return False
 
-        if self.dutycycle < self.__min_dutycycle__:
-            self.dutycycle = self.__min_dutycycle__
-
-        print(f'Setting duty cycle to {self.dutycycle}')
-        self.pi.set_PWM_dutycycle(self.pwm_gpio, self.dutycycle)
+        self.duty_cycle = new_duty_cycle
+        print(f'Setting duty cycle to {self.duty_cycle}')
+        self.pi.set_PWM_dutycycle(self.pwm_gpio, self.duty_cycle)
         return True
-
-        return False
