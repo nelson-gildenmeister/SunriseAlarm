78bf13c8ba06ee6ff17b5eaa359cce9a542a2cfc
nelson-gildenmeister <nelson.gildenmeister@gmail.com>
2025-06-28 22:19:22 -0600

    checkpoint

--- sunrise_view.py
+++ sunrise_view.py
@@ -109,7 +109,7 @@ class OledDisplay:
                 self.draw.text((x, top + 0), first_line, font=self.font, fill=255)
                 self.draw.text((x, top + 8), second_line, font=self.font, fill=255)
                 self.draw.text((x, top + 16), third_line[idx:], font=self.font, fill=255)
-                self.draw.text((x, top + 25), fourth_line, font=self.font, fill=255)
+                #self.draw.text((x, top + 25), fourth_line, font=self.font, fill=255)
 
                 if fourth_line != OledDisplay.display_line4:
                     fourth_line = OledDisplay.display_line4
